e861dfa9f971ace4b310e732c2827cb91db7e7ac
ajscimone <ajscimone@live.com>
2018-03-01 23:46:38 -0700

    dbcon again

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_scimonea',
-  password        : '0453',
+  password        : 'XXXX',
   database        : 'cs340_scimonea'
 });
 module.exports.pool = pool;
